5499f358175374f77322dc8af753f51c4a827a8d
Sven <SvePu@users.noreply.github.com>
2019-10-28 15:46:57 +0100

    Add files via upload

---
+++ UPLOAD/inc/languages/deutsch_sie/admin/amnesia.lang.php
@@ -0,0 +1,38 @@
+<?php
+/*****************************************/
+/****** German translation by SvePu ******/
+/****** Lang File admin/deutsch_sie ******/
+/************* Amnesia PlugIn ************/
+
+$l['amnesia_admin_pluginlibrary_missing'] = 'Fügen Sie <a href="https://github.com/frostschutz/MyBB-PluginLibrary"> PluginLibrary </a> hinzu, um das Plugin zu verwenden';
+
+$l['amnesia_admin_personal_data_erasure_requests'] = 'Datenlöschanfragen';
+$l['amnesia_admin_personal_data_erasure_requests_unapproved'] = 'Nicht genehmigte Anfragen';
+$l['amnesia_admin_personal_data_erasure_requests_unapproved_description'] = 'In diesem Bereich können Sie Löschanforderungen für persönliche Daten verwalten.';
+
+$l['amnesia_admin_personal_data_erasure_requests_pending'] = 'Offene Anfragen';
+$l['amnesia_admin_personal_data_erasure_requests_pending_description'] = 'In diesem Bereich können Sie ausstehende Löschanforderungen anzeigen.';
+
+$l['amnesia_admin_personal_data_erasure_requests_completed'] = 'Abgeschlossene Anfragen';
+$l['amnesia_admin_personal_data_erasure_requests_completed_description'] = 'In diesem Bereich können Sie abgeschlossene Löschanforderungen anzeigen.';
+
+$l['amnesia_admin_user'] = 'Benutzer';
+$l['amnesia_admin_user_id'] = 'ID {1}';
+$l['amnesia_admin_date'] = 'Datum';
+$l['amnesia_admin_scheduled_date'] = 'Geplant für';
+$l['amnesia_admin_action_date'] = 'Gelöscht';
+$l['amnesia_admin_with_content'] = 'Inhalt';
+$l['amnesia_admin_comment'] = 'Kommentar';
+$l['amnesia_admin_approve'] = 'Genehmigen';
+$l['amnesia_admin_cancel'] = 'Abbrechen';
+$l['amnesia_admin_personal_data_erasure_requests_unapproved_empty'] = 'Keine Anfragen, deren Genehmigung noch aussteht.';
+$l['amnesia_admin_personal_data_erasure_requests_pending_empty'] = 'Keine ausstehenden Anfragen.';
+$l['amnesia_admin_personal_data_erasure_requests_completed_empty'] = 'Keine abgeschlossenen Anfragen.';
+$l['amnesia_admin_personal_data_erasure_approve_confirm_title'] = 'Bestätigen Sie die Genehmigung der Anforderung';
+$l['amnesia_admin_personal_data_erasure_approve_confirm_message'] = 'Möchten Sie die ausgewählte Löschanforderung wirklich genehmigen?';
+$l['amnesia_admin_personal_data_erasure_cancel_confirm_title'] = 'Bestätigen Sie die Stornierung der Anfrage';
+$l['amnesia_admin_personal_data_erasure_cancel_confirm_message'] = 'Möchten Sie die ausgewählte Löschanforderung wirklich abbrechen?';
+$l['amnesia_admin_personal_data_erasure_request_approved'] = 'Die ausgewählte Anfrage wurde erfolgreich genehmigt.';
+$l['amnesia_admin_personal_data_erasure_request_canceled'] = 'Die ausgewählte Anfrage wurde erfolgreich abgebrochen.';
+
+$l['amnesia_erasure_task_ran'] = 'Die Aufgabe Amnesia (Löschen) wurde erfolgreich ausgeführt.';
---
+++ UPLOAD/inc/languages/deutsch_sie/amnesia.lang.php
@@ -0,0 +1,66 @@
+<?php
+/*****************************************/
+/****** German translation by SvePu ******/
+/********* Lang File deutsch_sie *********/
+/************* Amnesia PlugIn ************/
+
+$l['amnesia_privacy_policy_agreement_required'] = 'Bitte lesen und akzeptieren Sie die Datenschutzbestimmungen, bevor Sie die Website nutzen.';
+$l['amnesia_privacy_policy_signup_statement'] = 'Wenn Sie fortfahren, stimmen Sie den <a href="{1}"> Datenschutzbestimmungen </a> zu.';
+$l['amnesia_privacy_policy_accept'] = 'Akzeptieren';
+$l['amnesia_privacy_policy_date'] = 'Letzte Aktualisierung {1}';
+$l['amnesia_privacy_policy_date_last_read'] = 'Akzeptiert {1}';
+$l['amnesia_privacy_policy_accepted'] = 'Vielen Dank, Sie werden jetzt zurückgeleitet.';
+
+$l['amnesia_personal_data_erasure'] = 'Löschung personenbezogener Daten';
+$l['amnesia_personal_data_erasure_description'] = 'Hier können Sie die Löschung Ihrer persönlichen Daten beantragen, wodurch Ihr Konto <strong> dauerhaft deaktiviert </strong> wird. Ein Bestätigungscode wird an Ihre E-Mail-Adresse gesendet, um Ihre Anfrage zu bestätigen.';
+$l['amnesia_personal_data_erasure_waiting_period_days'] = 'Die Daten werden nach {1} Tagen gelöscht. Während dieser Zeit können Sie den Vorgang nach der Anmeldung abbrechen.';
+$l['amnesia_personal_data_erasure_approval'] = 'Ihre Daten werden gelöscht, sobald der Administrator Ihre Anfrage genehmigt hat.';
+$l['amnesia_personal_data_erasure_options'] = 'Löschoptionen';
+$l['amnesia_personal_data_erasure_content'] = 'Geposteten Inhalt entfernen';
+$l['amnesia_personal_data_erasure_content_description'] = 'Wählen Sie aus, ob auch von Ihrem Account gepostete Inhalte (Posts, Threads usw.) entfernt werden sollen.';
+$l['amnesia_personal_data_erasure_comment'] = 'Kommentar';
+$l['amnesia_personal_data_erasure_comment_description'] = 'Sie können zusätzliche Details in Bezug auf Ihre Anfrage an den Board-Administrator angeben.';
+$l['amnesia_personal_data_erasure_proceed'] = 'Löschen anfordern';
+$l['amnesia_personal_data_erasure_no_super_admin'] = 'Bitte entfernen Sie Ihr Konto aus der Liste der Superadministratoren, um die Löschung der Daten anzufordern.';
+$l['amnesia_personal_data_erasure_requested'] = 'Ein Bestätigungscode wurde an Ihre E-Mail-Adresse gesendet.';
+$l['amnesia_personal_data_erasure_request_verification_subject'] = 'Bestätigen Sie Ihre Anfrage zum Löschen persönlicher Daten';
+$l['amnesia_personal_data_erasure_request_verification_message'] = '{1},
+
+Um den Löschvorgang für personenbezogene Daten bei {2} abzuschließen, müssen Sie in Ihrem Webbrowser die unten stehende URL aufrufen. Der Code ist 24 Stunden gültig.
+
+{3}
+
+Vielen Dank,
+{2} Mitarbeiter';
+$l['amnesia_personal_data_erasure_pending'] = 'Löschen persönlicher Daten ausstehend';
+$l['amnesia_personal_data_erasure_pending_message'] = 'Sie haben die Löschung personenbezogener Daten beantragt.';
+$l['amnesia_personal_data_erasure_scheduled_for'] = 'Die Löschung ist für {1} geplant.';
+$l['amnesia_personal_data_erasure_cancel'] = 'Löschen abbrechen';
+
+$l['amnesia_personal_data_erasure_awaiting_approval_single'] = 'Es gibt 1 Löschanforderung für personenbezogene Daten, die noch genehmigt werden muss. Bitte gehen Sie zu Ihrem ACP, um die Anfrage zu überprüfen.';
+$l['amnesia_personal_data_erasure_awaiting_approval_plural'] = 'Es stehen {1} Löschanforderungen für personenbezogene Daten zur Genehmigung an. Bitte gehen Sie zu Ihrem ACP, um die Anfragen zu überprüfen.';
+$l['amnesia_personal_data_erasure_awaiting_approval_link'] = ' <a href="{1}">Gehen Sie zum ACP</a>.';
+
+
+$l['amnesia_personal_data_export'] = 'Export personenbezogener Daten';
+$l['amnesia_personal_data_export_description'] = 'Hier können Sie eine Kopie Ihrer persönlichen Daten in einem maschinenlesbaren XML-Format anfordern. Ein Bestätigungscode wird an Ihre E-Mail-Adresse gesendet, um Ihre Anfrage zu bestätigen.';
+$l['amnesia_personal_data_export_key_invalid'] = 'Auf den Bestätigungslink muss mit demselben Gerät und derselben Browsersitzung zugegriffen werden. Bitte fordern Sie erneut einen Datenexport an.';
+$l['amnesia_personal_data_export_proceed'] = 'Fortsetzen';
+$l['amnesia_personal_data_export_requested'] = 'Ein Bestätigungscode wurde an Ihre E-Mail-Adresse gesendet.';
+$l['amnesia_personal_data_export_request_verification_subject'] = 'Bestätigen Sie Ihre Anfrage zum Exportieren persönlicher Daten';
+$l['amnesia_personal_data_export_request_verification_message'] = '{1},
+
+Um den Exportvorgang für personenbezogene Daten um {2} abzuschließen, müssen Sie in dem Webbrowser, den Sie zum Initiieren der Anforderung verwendet haben, zur folgenden URL wechseln. Der Code ist 24 Stunden gültig.
+
+{3}
+
+Vielen Dank,
+{2} Mitarbeiter';
+$l['amnesia_personal_data_export_verify'] = 'Sie können nun eine Datei mit persönlichen Informationen zu Ihrem Konto herunterladen.';
+$l['amnesia_personal_data_export_download'] = 'Herunterladen';
+
+$l['amnesia_verification_code_invalid'] = 'Der Bestätigungscode ist ungültig oder abgelaufen.';
+
+$l['amnesia_anonymized_username_prefix'] = 'Benutzer ';
+
+$l['amnesia_erasure_task_ran'] = 'Die Amnesia-Löschaufgabe wurde erfolgreich ausgeführt.';
